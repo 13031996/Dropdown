4f20a006b337982d870187647eebde758a95e403
Shalu <ch.shalu13@gmail.com>
2022-11-24 12:31:47 +0530

    Initial Commit

---
+++ src/components/MultiSelector.js
@@ -0,0 +1,91 @@
+import React from "react";
+import Select from "react-select";
+import "./multiSelector.css";
+import chroma from "chroma-js";
+
+const MultiSelector = () => {
+  const options = [
+    { value: "ocean", label: "Ocean", color: "#00B8D9" },
+    { value: "blue", label: "Blue", color: "#0052CC" },
+    { value: "purple", label: "Purple", color: "#5243AA" },
+    { value: "red", label: "Red", color: "#FF5630" },
+    { value: "orange", label: "Orange", color: "#FF8B00" },
+    { value: "yellow", label: "Yellow", color: "#FFC400" },
+    { value: "green", label: "Green", color: "#36B37E" },
+    { value: "forest", label: "Forest", color: "#00875A" },
+    { value: "slate", label: "Slate", color: "#253858" },
+    { value: "silver", label: "Sliver", color: "#666666" },
+  ];
+
+  const colorStyles = {
+    control: (styles) => ({ ...styles, backgroundColor: "white" }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      const color = chroma(data.color);
+      return {
+        ...styles,
+        color: data.color,
+        backgroundColor: isDisabled
+          ? undefined
+          : isSelected
+          ? data.color
+          : isFocused
+          ? color.alpha(0.1).css()
+          : undefined,
+        color: isDisabled
+          ? "#ccc"
+          : isSelected
+          ? chroma.contrast(color, "white") > 2
+            ? "white"
+            : "black"
+          : data.color,
+        cursor: isDisabled ? "not-allowed" : "default",
+
+        ":active": {
+          ...styles[":active"],
+          backgroundColor: !isDisabled
+            ? isSelected
+              ? data.color
+              : color.alpha(0.3).css()
+            : undefined,
+        },
+      };
+    },
+    multiValue: (styles, { data }) => {
+      const color = chroma(data.color);
+      return {
+        ...styles,
+        backgroundColor: color.alpha(0.1).css(),
+      };
+    },
+    multiValueLabel: (styles, { data }) => ({
+      ...styles,
+      color: data.color,
+    }),
+    multiValueRemove: (styles, { data }) => ({
+      ...styles,
+      color: data.color,
+      ":hover": {
+        backgroundColor: data.color,
+        color: "white",
+      },
+    }),
+  };
+
+  return (
+    <>
+      <Select
+        className="select"
+        isMulti
+        options={options}
+        styles={colorStyles}
+        isClearable={true}
+        isSearchable={true}
+        isLoading={false}
+        isRtl={false}
+        closeMenuOnSelect={false}
+      />
+    </>
+  );
+};
+
+export default MultiSelector;
